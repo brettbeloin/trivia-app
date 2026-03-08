e0604771331a19705a163e0d829a5915f4c89ef9
Brett Beloin <brettbeloin@gmail.com>
2026-03-08 14:55:33 -0600

    add basic window

---
+++ include/window.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <raylib.h>
+
+const char *text = "Hello Trivia";
+int font_size = 20;
+int text_width;
+
+int height = 800;
+int width = 800;
--- src/main.c
+++ src/main.c
@@ -1,35 +1,21 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-void fiz();
+#include "window.h"
+#include <raylib.h>
 
 int main(int argc, char *argv[]) {
-  int age = 22;
+  text_width = MeasureText(text, font_size);
+  int x = (width - text_width) / 2;
+  int y = height / 2;
 
-  if (age >= 21) {
-    printf("you are %d years old. you can come in.\n", age);
-  } else {
-    printf("you are %d years old. you can not come in.\n", age);
+  InitWindow(width, height, "Trivia App");
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText(text, x, y, font_size, BLACK);
+    EndDrawing();
   }
 
-  fiz();
-
-  return EXIT_SUCCESS;
-}
-
-void fiz() {
-  int foo[10] = {1, 2, 3, 4, 5, 10, 12, 15, 18, 20};
-
-  for (int i = 0; i < (sizeof foo / sizeof foo[0]); i++) {
-    if (foo[i] % 15 == 0) {
-      printf("%d is a: FizzBuzz\n", foo[i]);
-    } else if (foo[i] % 5 == 0) {
-      printf("%d is a: Buzz\n", foo[i]);
-    } else if (foo[i] % 3 == 0) {
-      printf("%d is a: Fizz\n", foo[i]);
-    } else {
-      printf("%d\n", foo[i]);
-    }
-  }
+  CloseWindow();
+  return 0;
 }
